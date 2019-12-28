1a82d2fbb6a25c02e54c0ab681a7644460155515
dp07 <dp.madridiztazc@gmail.com>
2019-12-28 11:18:02 +0700

    update

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,6 +6,11 @@ class User extends CI_Controller
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        echo 'Selamat datang ' . $data['user']['name'];
+        // echo 'Selamat datang ' . $data['user']['name'];
+
+        $data['title'] = 'User Page';
+        $this->load->view('templates/header', $data);
+        $this->load->view('user/index', $data);
+        $this->load->view('templates/footer');
     }
 }
